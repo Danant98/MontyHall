35fa163b4181af358a0f7acb9da7ccbdde6b3e8c
Daniel E. Antonsen <danieleantonsen@gmail.com>
2023-05-19 14:02:57 +0200

    updated files

---
+++ src/gui.py
@@ -0,0 +1,26 @@
+
+__author__ = "Daniel Elisabethsønn Antonsen"
+
+import pygame
+
+
+
+class Userinterface:
+
+    def __init__(self):
+        pass
+
+    def start_screen(self):
+        pass
+    
+    def game_screen(self):
+        pass
+
+    def pause_screen(self):
+        pass
+
+    def end_screen(self):
+        pass
+
+    def scoreboard(self):
+        pass
---
+++ src/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+__author__ = "Daniel Elisabethsønn Antonsen"
+__version__ = "0.0.1"
+
+import numpy, sys, os, pygame
+
+class game_loop:
+
+    def __init__(self):
+        pass
+
+    def event(self):
+        pass
+
+    def run(self):
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    pass
