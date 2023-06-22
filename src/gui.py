763444aa657da4f027c06f6baddf39c55e02f6f0
Daniel E. Antonsen <danieleantonsen@gmail.com>
2023-06-22 20:51:08 +0200

    Updated files

--- src/gui.py
+++ src/gui.py
@@ -2,17 +2,21 @@ import pygame
 
 class Userinterface:
 
+    SCREEN_COLOR ='#335555'
     WHITE = '#FFFFFF'
+    RED = '#FF0000'
 
     def __init__(self, screen):
         self.screen = screen
+        self.screen_width, self.screen_height = self.screen.get_width(), self.screen.get_height() 
         self.username = None
         self.big_font = pygame.font.SysFont("Verdana", 30)
         self.little_font = pygame.font.SysFont("Verdana", 18)
+        self.host = pygame.transform.scale(pygame.image.load("../pictures/Monty_hall_host.jpg"), (50, 50))
+        
 
     def start_screen(self):
-        self.screen_width, self.screen_height = self.screen.get_width(), self.screen.get_height() 
-        self.screen.fill('#335555')
+        self.screen.fill(self.SCREEN_COLOR)
         welcome = self.big_font.render("WELCOMME TO MONTY HALL GAME", True, self.WHITE)
         start_game = self.little_font.render("START GAME", True, self.WHITE)
         scoreboard = self.little_font.render("SCOREBOARD", True, self.WHITE)
@@ -25,7 +29,9 @@ class Userinterface:
         
     
     def game_screen(self):
-        pass
+        self.screen.fill(self.SCREEN_COLOR)
+        self.screen.blit(self.host, (0, 0))
+
 
     def pause_screen(self):
         pass
@@ -34,4 +40,7 @@ class Userinterface:
         pass
 
     def scoreboard(self):
+        pass
+
+    def login(self, name:str):
         pass
--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ class game_loop:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.run_game = True
-        self.start_screen = True
+        self.start_screen = False
         self.SCREEN = pygame.display.set_mode((self.WIDTH, self.HEIGHT), pygame.RESIZABLE)
         pygame.display.set_caption("Monty Hall Game")
         self.UI = Userinterface(self.SCREEN)
@@ -26,26 +26,35 @@ class game_loop:
 
 
     def events(self):
+        """
+        Event handler
+        """
         for event in pygame.event.get():
-            if event.type == pygame.QUIT and self.start_screen:
+            if event.type == pygame.QUIT:
                 self.start_screen = False
                 self.run_game = False
-            if event.type == pygame.KEYDOWN and self.start_screen:
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.start_screen = False
                     self.run_game = False
 
     def run(self):
+        """
+        Main game loop
+        """
         while self.run_game:
+            # Setting FPS
             self.clock.tick(self.FPS)
+
+            # Setting start screen
             while self.start_screen:
                 self.clock.tick(self.FPS)
                 self.UI.start_screen()
                 self.events()
                 self.draw()
-                
-
-                
+            
+            # Running game
+            self.UI.game_screen()    
             self.events()
             self.draw()
         
